e96efe6cc1118c3a5208bef699d6e82c2d67351e
unknown <saroj.karki1675@gmail.com>
2020-09-15 20:13:19 -0400

    first commit

---
+++ src/test/java/com/gettingstarted/tests/MyFirstTest.java
@@ -0,0 +1,24 @@
+package com.gettingstarted.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Title;
+
+@RunWith(SerenityRunner.class)
+public class MyFirstTest {
+	
+	@Managed
+	WebDriver driver;
+	
+	@Title("This is my first serenity test method!!!!!")
+	@Test
+	public void navigateToGooglePage() throws InterruptedException {
+		
+		driver.navigate().to("https://www.google.com/");
+		Thread.sleep(3000);
+	}
+}
